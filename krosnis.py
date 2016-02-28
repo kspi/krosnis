56fc101840765a1d06543d9afd3a49cd2da4fbf4
kspi <k.spiridonovas@gmail.com>
2016-02-28 16:27:00 +0200

    Setpoint setting

--- krosnis.py
+++ krosnis.py
@@ -9,15 +9,20 @@ import struct
 from collections import namedtuple
 import time
 import csv
+import math
+from model import model
 
 
-class Status(namedtuple('Status', ['time', 'local_time', 'power', 'temp_outside', 'temp_inside'])):
+class Status(namedtuple('Status', ['time', 'local_time', 'power', 'setpoint', 'temp_outside', 'temp_inside'])):
     @property
-    def delta_temp(self):
+    def temp(self):
         return self.temp_inside - self.temp_outside
 
 
 START_TIME = time.time()
+
+def local_time():
+    return time.time() - START_TIME
 
 
 class Arduino:
@@ -29,15 +34,15 @@ class Arduino:
         self.started = threading.Event()
         self.last_status = None
         self._power = None
-        self.every_status = []
+        self._setpoint = None
 
     def line_status(self, line):
-        t_ms, pwm, temp_outside, temp_inside = line.strip().split(b',')
+        t_ms, pwm, setpoint, temp_outside, temp_inside = line.strip().split(b',')
         t = float(t_ms) / 1000.0
         p = float(pwm) / 255.0
         temp_outside = float(temp_outside)
         temp_inside = float(temp_inside)
-        return Status(t, time.time() - START_TIME, p, temp_outside, temp_inside)
+        return Status(t, local_time(), p, setpoint, temp_outside, temp_inside)
 
     def interact(self):
         with open(self.filename, 'wb') as f:
@@ -61,20 +66,9 @@ class Arduino:
                     self.status.put_nowait(status)
                     self.last_status = status
                     self._power = status.power
-                    self.every_status.append(status)
+                    self._setpoint = status.setpoint
             finally:
                 self.started.clear()
-
-    def time_deviation(self):
-        if self.every_status:
-            t0 = self.every_status[0].time
-            t0_local = self.every_status[0].local_time
-            t_sum = 0
-            for s in self.every_status:
-                t_sum += (s.time - t0) - (s.local_time - t0_local)
-            return t_sum / len(self.every_status)
-        else:
-            return 0
 
     def iter_status(self):
         assert(self.started.is_set())
@@ -102,6 +96,18 @@ class Arduino:
         command = struct.pack('cB', b'P', pwm)
         self.command.put(command)
 
+    @property
+    def setpoint(self):
+        assert(self.started.is_set())
+        return self._setpoint
+
+    @setpoint.setter
+    def setpoint(self, setpoint):
+        assert(self.started.is_set())
+        assert(0 <= setpoint <= 255)
+        x = int(setpoint)
+        command = struct.pack('cB', b'S', x)
+        self.command.put(command)
     def start(self):
         self.thread.start()
         self.started.wait()
@@ -119,7 +125,11 @@ class HeatPlot(tkplot.TkPlot):
         self.temp_inside_line, = self.plot.plot([], [], label="Inside temperature")
         self.temp_outside_line, = self.plot.plot([], [], label="Outside temperature")
         self.power_line, = self.plot.plot([], [], label="Power")
-        self.plot.legend(handles=[self.temp_inside_line, self.temp_outside_line, self.power_line])
+        self.setpoint_line, = self.plot.plot([], [], label="Setpoint")
+        self.plot.legend(handles=[self.temp_inside_line,
+                                  self.temp_outside_line,
+                                  self.power_line,
+                                  self.setpoint_line], bbox_to_anchor=(0.3, 1))
         self.figure.tight_layout()
 
     def update(self, status):
@@ -127,6 +137,7 @@ class HeatPlot(tkplot.TkPlot):
         temp_inside = [s.temp_inside for s in status]
         temp_outside = [s.temp_outside for s in status]
         power = [s.power * 100 for s in status]
+        setpoint = [s.setpoint for s in status]
 
         if time:
             self.plot.set_xlim(min(time), max(time))
@@ -137,6 +148,8 @@ class HeatPlot(tkplot.TkPlot):
             self.temp_outside_line.set_ydata(temp_outside)
             self.power_line.set_xdata(time)
             self.power_line.set_ydata(power)
+            self.setpoint_line.set_xdata(time)
+            self.setpoint_line.set_ydata(setpoint)
             self.figure.canvas.draw()
 
 
@@ -145,6 +158,8 @@ class Krosnis:
         self.root = root
         self.root.title("krosnis - {}".format(experiment))
         self.experiment = experiment
+
+        self.update_period = 1.0
 
         self.plot = HeatPlot(self.root)
         self.plot.pack(fill=tk.BOTH, expand=1)
@@ -163,13 +178,27 @@ class Krosnis:
         self.set_power = tk.Button(self.toolbar, text='Set power', command=self.set_power)
         self.set_power.pack(side=tk.LEFT)
 
+        self.setpoint_val = tk.StringVar()
+        self.setpoint_val.set('0.0')
+        self.setpoint = tk.Entry(self.toolbar, textvariable=self.setpoint_val)
+        self.setpoint.bind('<Return>', self.set_setpoint)
+        self.setpoint.pack(side=tk.LEFT)
+        self.setpoint.focus_set()
+        self.set_setpoint = tk.Button(self.toolbar, text='Set temperature', command=self.set_setpoint)
+        self.set_setpoint.pack(side=tk.LEFT)
+
         self.arduino = Arduino("experiments/{}_raw.csv".format(experiment))
+        self.every_status = []
+        self.th0 = 0
 
     def set_status(self, status):
         self.label.config(text=status)
 
     def set_power(self, event=None):
         self.arduino.power = float(self.power_val.get())
+
+    def set_setpoint(self, event=None):
+        self.arduino.setpoint = float(self.setpoint_val.get())
 
     def start(self):
         _self = self
@@ -180,18 +209,44 @@ class Krosnis:
         threading.Thread(target=shell, daemon=True).start()
         execute_delayed(self.root, self.sample())
 
+    def time_deviation(self):
+        if self.every_status:
+            t0 = self.every_status[0].time
+            t0_local = self.every_status[0].local_time
+            t_sum = 0
+            for s in self.every_status:
+                t_sum += (s.time - t0) - (s.local_time - t0_local)
+            return t_sum / len(self.every_status)
+        else:
+            return 0
+
+    def control(self):
+        pass
+        #p_supp, full_time, full_val = model.optimize_heating(self.th0, float(self.setpoint_val.get()), self.arduino.power)
+        #if full_time > 1:
+        #    self.arduino.power = full_val
+        #else:
+        #    self.arduino.power = p_supp
+
     def sample(self):
         self.arduino.start()
         with open("experiments/{}.csv".format(self.experiment), 'w') as f:
             csvf = csv.writer(f)
             csvf.writerow(Status._fields)
             while True:
-                for s in self.arduino.iter_status():
-                    csvf.writerow(s)
-                    self.set_status(str(s))
-                f.flush()
-                self.plot.update(self.arduino.every_status)
-                yield 0.1
+                try:
+                    for s in self.arduino.iter_status():
+                        if self.th0 is None:
+                            self.th0 = s.temp
+                        csvf.writerow(s)
+                        self.set_status(str(s))
+                        self.every_status.append(s)
+                    f.flush()
+                    self.plot.update(self.every_status)
+                    self.control()
+                    yield self.update_period
+                except Exception as e:
+                    print(e)
 
 
 def run(experiment):
